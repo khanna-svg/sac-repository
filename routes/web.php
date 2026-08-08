c8263b43f5739aa75ed841a04406883500614037
ryan <godzuuzumaki@gmail.com>
2026-08-09 03:38:40 +0800

    Semantic Search and RAG ChatBot Feature Working :)

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('login');
-});
-
-Route::get('/login', function () {
-    return view('login');
-})->name('login');
+Route::redirect('/', '/documents');
 
 Route::get('/documents', function () {
     return view('documents');
