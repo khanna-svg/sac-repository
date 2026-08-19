249eaaa3759b7a24e544c6927243de3fbf1a5992
ryan <godzuuzumaki@gmail.com>
2026-08-19 20:56:37 +0800

    trial 22

--- resources/views/document_detail.blade.php
+++ resources/views/document_detail.blade.php
@@ -99,7 +99,7 @@
                 <div id="tabAbstractContent" class="space-y-6">
                     <div>
                         <h2 class="text-sm font-bold uppercase tracking-wider text-[#700000] mb-2">Abstract</h2>
-                        <div class="rounded-2xl bg-slate-50 border border-gray-200 p-5 text-sm text-gray-700 leading-relaxed whitespace-pre-wrap font-sans">
+                        <div class="rounded-2xl bg-slate-50 border border-gray-200 p-5 text-sm text-gray-700 leading-relaxed whitespace-pre-wrap font-sans text-center">
                             {{ $document->abstract }}
                         </div>
                     </div>
